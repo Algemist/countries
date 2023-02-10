1d3ed3512de16a46854d1ce95eb229b6cfd2c3b6
Denis Davydov <denis6davydov23@gmail.com>
2023-02-11 00:46:57 +0300

    add entity types and theme variables

---
+++ src/types/country.ts
@@ -0,0 +1,31 @@
+import {Region} from "./regions";
+
+type Currency = {
+    code: string;
+    name: string;
+    symbol: string;
+};
+type Language = {
+    name: string;
+    nativeName: string;
+    iso639_1: string;
+    iso639_2: string;
+}
+
+export type Country = {
+    name: string;
+    nativeName: string;
+    flag: string;
+    flags: [
+        { png: string },
+        { svg: string },
+    ];
+    region: Region;
+    population: number;
+    subregion: string;
+    capital: string;
+    topLevelDomain: string[];
+    borders: string[];
+    currencies: Currency[];
+    languages: Language[];
+}
