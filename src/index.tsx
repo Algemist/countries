08fb6978a01d071c9ec794972b3d2ddb8a6847a2
Denis Davydov <denis6davydov23@gmail.com>
2023-02-11 23:12:08 +0300

    change basename in BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={"country"}>
+        <BrowserRouter basename={"countries"}>
             <Provider store={store}>
                 <App />
             </Provider>
